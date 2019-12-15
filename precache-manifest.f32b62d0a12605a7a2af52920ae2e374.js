f5694240bb972473801ad6bdd4109fefa66555c8
Ally Murray <allymurray88@gmail.com>
2019-12-15 18:44:51 +0000

    Updates

---
+++ precache-manifest.f32b62d0a12605a7a2af52920ae2e374.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1251a8decf6f4712d6a05670279abfb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e73f3c0f25cb589e853e",
+    "url": "/static/css/main.18e06cf8.chunk.css"
+  },
+  {
+    "revision": "c1f42301a7e6aa4ec935",
+    "url": "/static/js/2.62a87c34.chunk.js"
+  },
+  {
+    "revision": "65b307376583392f06f40dafae7dcc3f",
+    "url": "/static/js/2.62a87c34.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e73f3c0f25cb589e853e",
+    "url": "/static/js/main.1d56ed41.chunk.js"
+  },
+  {
+    "revision": "0ed8d05178c799c6f90d",
+    "url": "/static/js/runtime-main.8ba06fbe.js"
+  }
+]);
